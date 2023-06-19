c77a0933d345c75ba4b5cc18672a54238eb1a9ee
Vikash <ssvikash_20@Vikashs-MacBook-Pro.local>
2023-06-20 00:27:29 +0530

    Add firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:stem_quiz/auth.dart';
 
 ColorScheme colorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 242, 172, 66),
 );
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       home: const Auth(),
