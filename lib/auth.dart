aef5b74f4a486511036ff14bdbd5e19b254cd544
Vikash <ssvikash_20@Vikashs-MacBook-Pro.local>
2023-06-19 14:59:19 +0530

    Complete login screen

--- lib/auth.dart
+++ lib/auth.dart
@@ -6,27 +6,127 @@ class Auth extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TextEditingController emailC = TextEditingController();
+    final TextEditingController passC = TextEditingController();
 
     return Scaffold(
-      body: Column(
-        children: [
-          const Text("Stem Quiz"),
-          const Text("Login"),
-          TextField(
-            controller: emailC,
-            decoration: const InputDecoration(
-              hintText: "Your email",
-              prefixIcon: Icon(Icons.email),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 120,
             ),
-          ),
-          TextField(
-            controller: emailC,
-            decoration: const InputDecoration(
-              hintText: "Your Password",
-              prefixIcon: Icon(Icons.password)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.science,
+                  size: 50,
+                  color: Colors.orange
+                      .withOpacity(0.75),
+                ),
+                Text(
+                  "Quiz",
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                    color: Theme.of(context)
+                        .colorScheme
+                        .onPrimaryContainer
+                        .withOpacity(0.75),
+                  ),
+                ),
+                Icon(
+                  Icons.lightbulb_sharp,
+                  size: 50,
+                  color: Colors.orange
+                      .withOpacity(0.75),
+                ),
+              ],
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "Login",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 20, right: 20, left: 20, bottom: 10),
+              child: Card(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+                      child: TextField(
+                        controller: emailC,
+                        decoration: InputDecoration(
+                            hintText: "Your email",
+                            prefixIcon: const Icon(Icons.email),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30))),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: TextField(
+                        controller: passC,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                            hintText: "Your Password",
+                            prefixIcon: const Icon(Icons.password)),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.login,
+                        color: Theme.of(context).colorScheme.onPrimaryContainer,
+                      ),
+                      label: Text(
+                        "Sign in",
+                        style: TextStyle(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onPrimaryContainer),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(
+                            Theme.of(context).colorScheme.primaryContainer),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don't have an account?",
+                  style: TextStyle(fontWeight: FontWeight.w500),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text("Register"),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:stem_quiz/auth.dart';
 
+ColorScheme colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 242, 172, 66),
+);
+
 void main(List<String> args) {
   runApp(
     MaterialApp(
       home: const Auth(),
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(useMaterial3: true, colorScheme: colorScheme),
     ),
   );
 }
