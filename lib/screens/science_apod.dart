e5fc7cd554719ef59daff7b6171e8f404a01ba86
Vikash <ssvikash_20@Vikashs-MacBook-Pro.local>
2023-06-20 18:11:25 +0530

    Finish APOD screen(not the date picker)

--- lib/screens/science_apod.dart
+++ lib/screens/science_apod.dart
@@ -13,7 +13,78 @@ class _ScienceApodState extends State<ScienceApod> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        centerTitle: false,
+        title: const Text(
+          "APOD viewer",
+          style: TextStyle(fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                SizedBox(
+                  height: 300,
+                  width: double.infinity,
+                  child: Image.network(
+                    imageURL,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  color: Colors.black.withOpacity(0.25),
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 26,
+                        ),
+                      ),
+                      Text(
+                        "Date : $date",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            Text(
+              "Explanation",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w800,
+                color: Theme.of(context)
+                    .colorScheme
+                    .onSecondaryContainer
+                    .withOpacity(0.88),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 0, bottom: 0, left: 18, right: 18),
+              child: Text(
+                explanation,
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
