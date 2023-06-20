150ea7d20d927959d96ac7139344e5fe4c56452f
Vikash <ssvikash_20@Vikashs-MacBook-Pro.local>
2023-06-20 18:33:42 +0530

    Add cache without datepicker

--- lib/models/api_data.dart
+++ lib/models/api_data.dart
@@ -10,19 +10,41 @@ String title = "";
 String imageURL = "";
 String mediaType = "";
 
-fetchData(BuildContext context) async {
-  const url =
-      "https://api.nasa.gov/planetary/apod?api_key=jt19cWSthC8W1CmfyD3U5FEBzqzldmhOyz0Pa3Yh";
-  final uri = Uri.parse(url);
-  final response = await http.get(uri);
-  final body = response.body;
-  final json = jsonDecode(body);
+List<Map<dynamic, dynamic>> cache = [];
 
-  explanation = json["explanation"];
-  date = json["date"];
-  title = json["title"];
-  imageURL = json["hdurl"];
-  mediaType = json["media_type"];
+fetchData(BuildContext context) async {
+  if (cache.isEmpty) {
+    const url =
+        "https://api.nasa.gov/planetary/apod?api_key=jt19cWSthC8W1CmfyD3U5FEBzqzldmhOyz0Pa3Yh";
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+
+    explanation = json["explanation"];
+    date = json["date"];
+    title = json["title"];
+    imageURL = json["hdurl"];
+    mediaType = json["media_type"];
+
+    final tempCache = {};
+    tempCache["explanation"] = explanation;
+    tempCache["date"] = date;
+    tempCache["title"] = title;
+    tempCache["imageURL"] = imageURL;
+    tempCache["mediaType"] = mediaType;
+
+    if (!(cache.contains(tempCache))) {
+      cache.add(tempCache);
+    }
+  }
+  else {
+    explanation = cache[0]["explanation"];
+    date = cache[0]["date"];
+    title = cache[0]["title"];
+    imageURL = cache[0]["imageURL"];
+    mediaType = cache[0]["mediaType"];
+  }
 
   if (context.mounted) {
     Navigator.push(
